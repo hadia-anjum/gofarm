5bd3b503c8cb367a06aea7dd15f275f72d3f2a9d
Hadiaa4567 <hadiaa.anjum@gmail.com>
2026-07-07 20:29:12 +0500

    fix: add global error boundary and disable production source maps

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,17 @@
-// Trigger Vercel deployment
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-  devIndicators: false,
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    unoptimized: true,
+  },
+  // Yeh line automatic system prefetching loop ko block kar degi
+  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
